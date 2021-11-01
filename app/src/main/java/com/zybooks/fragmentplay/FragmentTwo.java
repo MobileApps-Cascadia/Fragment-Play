9fed742e880cf83712f35f37c6f9bc4c56d1ff53
Brian Bansenauer <brianb@cascadia.edu>
2021-11-01 12:36:22 -0700

    adds fragment two

---
+++ app/src/main/java/com/zybooks/fragmentplay/FragmentTwo.java
@@ -0,0 +1,17 @@
+package com.zybooks.fragmentplay;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+    public FragmentTwo() {
+        super(R.layout.fragment_two);
+    }
+
+}
