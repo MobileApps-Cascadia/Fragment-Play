7286bfdc977c7562f66e47567bde704606373844
Brian Bansenauer <brianb@cascadia.edu>
2021-11-01 12:29:47 -0700

    initial

---
+++ app/src/main/java/com/zybooks/fragmentplay/FragmentOne.java
@@ -0,0 +1,11 @@
+package com.zybooks.fragmentplay;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentOne extends Fragment {
+
+        public FragmentOne() {
+            super(R.layout.fragment_one);
+        }
+
+}
