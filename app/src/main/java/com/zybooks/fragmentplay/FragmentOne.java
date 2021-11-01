927bd954cc96fd54347e14fb37a13f45cbdc680c
Brian Bansenauer <brianb@cascadia.edu>
2021-11-01 13:27:07 -0700

    corrects fragment layout & color

--- app/src/main/java/com/zybooks/fragmentplay/FragmentOne.java
+++ app/src/main/java/com/zybooks/fragmentplay/FragmentOne.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,10 +13,7 @@ public class FragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+       // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one, container, false);
-
-
     }
-
 }
--- app/src/main/java/com/zybooks/fragmentplay/MainActivity.java
+++ app/src/main/java/com/zybooks/fragmentplay/MainActivity.java
@@ -1,16 +1,19 @@
 package com.zybooks.fragmentplay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction()
+        fm = getSupportFragmentManager();
+        fm.beginTransaction()
                 .setReorderingAllowed(true)
                 .add(R.id.fragment_container_view, FragmentOne.class, null)
                 .commit();
