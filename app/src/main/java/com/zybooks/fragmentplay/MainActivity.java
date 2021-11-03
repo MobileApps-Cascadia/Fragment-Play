365750e6b524e72102cbc5c9894750c6eb207579
Isaak Wheeler <isaak.wheeler@hotmail.com>
2021-11-02 19:55:09 -0700

    Isaak and Andrea adding fragment swap

--- app/src/main/java/com/zybooks/fragmentplay/MainActivity.java
+++ app/src/main/java/com/zybooks/fragmentplay/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,16 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         fm = getSupportFragmentManager();
         //TODO: Use the fragment manager and a transaction to add FragmentOne to the Activity's fragment container view
+        fm.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, com.zybooks.fragmentplay.FragmentOne.class, null)
+                .addToBackStack("FragmentOne")
+                .commit();
 
 
         Button swap = findViewById(R.id.swap_button);
         //TODO: Add onClickListener that calls swap
+        swap.setOnClickListener(v -> swap());
 
     }
 
     public void swap() {
         //TODO: Use Transactions and the BackStack to swap between FragmentOne and FragmentTwo
 
+        fm.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, com.zybooks.fragmentplay.FragmentTwo.class, null)
+                .addToBackStack("FragmentTwo")
+                .commit();
     }
 
 }
